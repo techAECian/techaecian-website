18245235c2e374d6a426582f753fafe10379718b
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-02-04 14:20:40 +0530

    working on HomePage introSection

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.css'
 import headerImage from '../../assets/home/header.svg'
+import introImage from '../../assets/home/intro.svg'
 const Home = () => {
     return (
         <div className="aec__home section-padding">
@@ -15,6 +16,15 @@ const Home = () => {
                 </div>
                 <div className="aec__home-header__image">
                     <img src={headerImage} alt="" srcset="" />
+                </div>
+            </div>
+            <div className="aec__home-intro section-padding">
+                <div className="aec__home-intro__image">
+                    <img src={introImage} alt="" />
+                </div>
+                <div className="aec__home-intro__content">
+                    <h1>Who are we?</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, similique sapiente. Tempora eius dolor iste inventore nemo quidem qui repellendus nihil, accusamus doloribus facere alias ex dolores. Odio alias voluptatem consectetur molestiae possimus ex, maiores asperiores aliquid, illo, unde aspernatur pariatur esse nam reiciendis error laborum mollitia itaque velit in optio aliquam autem reprehenderit tempora repellendus. Voluptas in nam magnam.</p>
                 </div>
             </div>
         </div>
